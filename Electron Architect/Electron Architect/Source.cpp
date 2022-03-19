e9deeae812ed91425d7d9d630906d6b0578fcf59
Amy <henrythepony@gmail.com>
2022-03-19 16:58:41 -0400

    Finally got the project setup

--- Electron Architect/Electron Architect/Source.cpp
+++ Electron Architect/Electron Architect/Source.cpp
@@ -1,7 +1,60 @@
 #include <raylib.h>
 
+class Node;
+
+struct Wire
+{
+    Node* start;
+    Node* end;
+};
+
+class Node
+{
+
+};
+
 int main()
 {
+    int windowWidth = 1280;
+    int windowHeight = 720;
+    InitWindow(windowWidth, windowHeight, "My Raylib Program");
+    SetTargetFPS(60);
+
+    /******************************************
+    *   Load textures, shaders, and meshes
+    ******************************************/
+
+    // TODO: Load persistent assets & variables
+
+    while (!WindowShouldClose())
+    {
+        /******************************************
+        *   Simulate frame and update variables
+        ******************************************/
+
+        // TODO: simulate frame
+
+        /******************************************
+        *   Draw the frame
+        ******************************************/
+
+        BeginDrawing(); {
+
+            ClearBackground(BLACK);
+
+            // TODO: Draw frame
+
+        } EndDrawing();
+    }
+
+    /******************************************
+    *   Unload and free memory
+    ******************************************/
+
+    // TODO: Unload variables
+
+    CloseWindow();
+
 	return 0;
 }
 
