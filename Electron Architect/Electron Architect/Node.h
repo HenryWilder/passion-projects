0d9352c5286c5d4b737c385acdccd3aa06a57038
Amy <henrythepony@gmail.com>
2022-04-25 16:42:46 -0400

    Simplified enum values for gates

--- Electron Architect/Electron Architect/Node.h
+++ Electron Architect/Electron Architect/Node.h
@@ -5,16 +5,50 @@ struct Wire;
 
 enum class Gate : char
 {
-    OR = '|',
-    AND = '&',
-    NOR = '!',
-    XOR = '^',
+    OR,
+    AND,
+    NOR,
+    XOR,
 
-    RESISTOR = '~',
-    CAPACITOR = '=',
-    LED = '@',
-    DELAY = ';',
+    RESISTOR,
+    CAPACITOR,
+    LED,
+    DELAY,
 };
+constexpr char GateToChar(Gate gate)
+{
+    switch (gate)
+    {
+        ASSERT_SPECIALIZATION;
+
+    case Gate::OR: return '|';
+    case Gate::AND: return '&';
+    case Gate::NOR: return '!';
+    case Gate::XOR: return '^';
+
+    case Gate::RESISTOR: return '~';
+    case Gate::CAPACITOR: return '=';
+    case Gate::LED: return '@';
+    case Gate::DELAY: return ';';
+    }
+}
+constexpr Gate CharToGate(char symbol)
+{
+    switch (symbol)
+    {
+        ASSERT_SPECIALIZATION;
+
+    case '|': return Gate::OR;
+    case '&': return Gate::AND;
+    case '!': return Gate::NOR;
+    case '^': return Gate::XOR;
+
+    case '~': return Gate::RESISTOR;
+    case '=': return Gate::CAPACITOR;
+    case '@': return Gate::LED;
+    case ';': return Gate::DELAY;
+    }
+}
 
 class Node
 {
