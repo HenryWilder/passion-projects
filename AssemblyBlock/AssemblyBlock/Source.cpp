63d48cd4cc6291096db50f16b6e0936c8c938299
Henry Wilder <Henry>
2022-06-08 23:26:41 -0700

    Got started

---
+++ AssemblyBlock/AssemblyBlock/Source.cpp
@@ -0,0 +1,31 @@
+#include <raylib.h>
+
+int main()
+{
+	InitWindow(1280, 720, "Assembly Block v0.0.1");
+#pragma region Prep phase
+
+#pragma endregion
+#pragma region Program loop
+	while (!WindowShouldClose())
+	{
+#pragma region Sim phase
+		{
+
+		}
+#pragma endregion
+#pragma region Draw phase
+		BeginDrawing();
+		{
+
+		}
+		EndDrawing();
+#pragma endregion
+	}
+#pragma endregion
+#pragma region Cleanup phase
+
+#pragma endregion
+	CloseWindow();
+	return 0;
+}
